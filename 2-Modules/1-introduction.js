97c3f6453417fb1a7405960f2fe3c0c525b6d74c
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-01-10 15:00:51 +0545

    Day 02

---
+++ 2-Modules/1-introduction.js
@@ -0,0 +1,17 @@
+// Modules
+// Consider modules to be the same as JavaScript libraries.
+// A set of functions you want to include in your application.
+
+// Built-in Modules
+// Node.js has a set of built-in modules which you can use without any further installation.
+// to include a module, you need to use require(filepath) function
+// For example:
+// 1. path - To handle file paths
+// 2. os - Provides information about the operation system
+// 3. fs - To handle the file system
+// 4. http/https - To make Node.js act as an HTTP/HTTPs server
+// 5. url - To parse URL strings
+
+// Custom Modules
+// You can create your own modules, and easily include them in your applications.
+// Notice that we use ./ to locate the module
---
+++ 2-Modules/2-example.js
@@ -0,0 +1,19 @@
+// 1.
+const text = 'Hello, I am Bibash';
+module.exports.text = text;
+
+// 2.
+const myFunction = (a,b) => {
+    return a+b;
+}
+module.exports.myFunction = myFunction;
+
+// 3.
+module.exports.items = ['item1', 'item2'];
+
+// 4.
+module.exports.myObject = {
+    id: 1516160,
+    name: 'Ram',
+    age: 20
+}

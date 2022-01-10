c4d57f9c7e666a27f376e64f6fc745280fd12f11
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-01-10 13:51:22 +0545

    Day 01

---
+++ 1-Introduction/1-getting-started.js
@@ -0,0 +1,9 @@
+console.log('Hello world');
+console.log(2+3);
+console.log("K" + " " + "Cha" + " " + "?");
+
+// Adding two numbers
+let x = 5;
+let y = 10;
+
+console.log(x+y);
---
+++ 1-Introduction/2-introduction.js
@@ -0,0 +1,14 @@
+// some important terms
+
+// __dirname - path to current directory
+// __filename - filepath
+// require - funtion to use modules
+// module - info about current module
+// process - info about env where the program is executed
+
+console.log(module);
+console.log(__dirname);
+console.log(module.path);
+console.log(__filename);
+console.log(module.filename);
+console.log(module.exports);

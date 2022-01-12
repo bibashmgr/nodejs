b72508ed812f72fb7b54cde2c1d1d8c171856f29
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-01-13 00:03:40 +0545

    Day 04

--- 2-Modules/3-fs-module/1-intro.js
+++ 2-Modules/3-fs-module/1-intro.js
@@ -17,17 +17,11 @@ const fs = require('fs');
 // path: filename or file descriptor
 // options: object containing encoding & flag
 
-const firstTextAsync = fs.readFileSync(
-  './2-Modules/3-fs-module/temp/first.txt',
-  'utf-8'
-);
-console.log(firstTextAsync);
+const firstTextSync = fs.readFileSync('./temp/first.txt', 'utf-8');
+console.log(firstTextSync);
 
-const secondTextAsync = fs.readFileSync(
-  './2-Modules/3-fs-module/temp/second.txt',
-  'utf-8'
-);
-console.log(secondTextAsync);
+const secondTextSync = fs.readFileSync('./temp/second.txt', 'utf-8');
+console.log(secondTextSync);
 
 // 2. fs.writeFileSync(file, data, [options])
 // options: object containing encoding, mode & flag
@@ -35,13 +29,71 @@ console.log(secondTextAsync);
 // if the file is already exist, then it will overwrite the content.
 // if the file isnot exist, then it will create a new file and add the content.
 
-const thirdTextContent = `Here is the content of third.txt file: ${firstTextAsync} || ${secondTextAsync}`;
+const thirdTextContent = `Here is the content of third.txt file: ${firstTextSync} || ${secondTextSync}`;
 console.log(thirdTextContent);
 
-fs.writeFileSync('./2-Modules/3-fs-module/temp/third.txt', thirdTextContent);
+fs.writeFileSync('./temp/third.txt', thirdTextContent);
 // the above statement overwrites the content in third.txt file
 
-fs.writeFileSync('./2-Modules/3-fs-module/temp/fourth.txt', thirdTextContent, {
+fs.writeFileSync('./temp/fourth.txt', thirdTextContent, {
   flag: 'a',
 });
 // the above statement appends the new content with old content in fourth.txt file
+
+// Synchronous API:
+
+// 1. fs.readFile(path, [options], callback)
+// callback: callback funtion that handles error and result
+fs.readFile('./temp/first.txt', 'utf-8', (error, result) => {
+  if (error) {
+    console.log(error);
+    return null;
+  }
+  console.log(result);
+});
+
+fs.readFile('./temp/second.txt', 'utf-8', (error, result) => {
+  if (error) {
+    console.log(error);
+    return null;
+  }
+  console.log(result);
+});
+
+fs.readFile('./temp/first.txt', 'utf-8', (error, result) => {
+  if (error) {
+    console.log(error);
+    return null;
+  }
+  // console.log(result);
+  const firstTextAsync = result;
+
+  fs.readFile('./temp/second.txt', 'utf-8', (err, res) => {
+    if (err) {
+      console.log(err);
+      return null;
+    }
+    // console.log(res);
+    const secondTextAsync = res;
+
+    fs.writeFile(
+      './temp/fifth.txt',
+      ` Using Synchronous API: ${firstTextAsync} || ${secondTextAsync}`,
+      { flag: 'a' },
+      (er, re) => {
+        if (er) {
+          console.log(e);
+          return null;
+        }
+
+        fs.readFile('./temp/fifth.txt', 'utf-8', (e, r) => {
+          if (e) {
+            console.log(e);
+            return null;
+          }
+          console.log(r);
+        });
+      }
+    );
+  });
+});
--- 2-Modules/index.js
+++ 2-Modules/index.js
@@ -1,12 +1,12 @@
-const example = require('./2-Modules/2-example');
+const example = require('./2-example');
 
 console.log(example.text);
 
-console.log(example.myFunction(15,24));
+console.log(example.myFunction(15, 24));
 
 example.items.map((item) => {
-    console.log(item);
+  console.log(item);
 });
 
-const {id, name, age} = example.myObject;
-console.log(id,name,age);
+const { id, name, age } = example.myObject;
+console.log(id, name, age);
